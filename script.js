9c237544c70ad0816934e18fab4d80b6189e3bd5
Ishankpathak <pathakishank4@gmail.com>
2023-02-19 11:35:42 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// function for screen to top
+let myButton = document.getElementById("myBtn");
+
+function topFunction(){
+    document.documentElement.scrollTop = 10;
+}
+
+// document.getElementById("myBtn").click();
+// function myFunction(id) {
+//   var x = document.getElementById(id);
+//   if (x.className.indexOf("w3-show") == -1) {
+//     x.className += " w3-show";
+//   } else { 
+//     x.className = x.className.replace(" w3-show", "");
+//   }
+// }
+
+// function likeFunction(x) {
+//   x.style.fontWeight = "bold";
+//   x.innerHTML = "✓ Liked";
+// }
+
